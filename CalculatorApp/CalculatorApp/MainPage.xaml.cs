6dfdb35bce11cc40f34b14f059f06c73bd3d886d
Reinielle Matt <erecereinielle722@gmail.com>
2018-01-13 18:44:16 +0800

    Layout created

--- CalculatorApp/CalculatorApp/MainPage.xaml.cs
+++ CalculatorApp/CalculatorApp/MainPage.xaml.cs
@@ -9,9 +9,32 @@ namespace CalculatorApp
 {
 	public partial class MainPage : ContentPage
 	{
+        private string displayValue = "0";
+        private double firstNumber, secondNumber;
+        private char operation;
+        private bool anOperationClicked = false;
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+            CButton.Clicked += (sender, e) =>
+            {
+                displayValue = "0";
+                DisplayLabel.Text = displayValue;
+            };
+
 		}
-	}
+
+        public void NumberButtonEvent(Object sender, EventArgs e)
+        {
+            Button ClickedButton = (Button)sender;
+            if (displayValue == "0")
+                displayValue = ClickedButton.Text;
+            else
+                displayValue += ClickedButton.Text;
+
+            DisplayLabel.Text = displayValue;
+        }
+    }
 }
